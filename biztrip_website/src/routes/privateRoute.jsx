b9873d1958ae5dc10eccb01cea382642aa5812bb
Duc Anh <ducanhlekha2802@gmail.com>
2023-04-05 06:30:45 +0700

    create structure project

---
+++ biztrip_website/src/routes/privateRoute.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Navigate} from "react-router-dom";
+
+const PrivateRoute = (props) => {
+    // const isAuthenticated = useSelector(state => state.user.isAuthenticated);
+    const isAuthenticated = true;
+    return (
+        isAuthenticated ? props.children : <Navigate to={'/login'}></Navigate>
+    );
+};
+
+export default PrivateRoute;
