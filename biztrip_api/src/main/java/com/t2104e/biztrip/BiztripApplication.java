cae64d9a65368c2c934d49bd34c2b9f1191321c2
yotsubasuzu <74945325+yotsubasuzu@users.noreply.github.com>
2023-05-04 08:46:59 +0700

    initial commit

---
+++ biztrip_api/src/main/java/com/t2104e/biztrip/BiztripApplication.java
@@ -0,0 +1,13 @@
+package com.t2104e.biztrip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BiztripApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BiztripApplication.class, args);
+	}
+
+}
