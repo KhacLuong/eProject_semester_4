c0c6003517537a5371751bd5552fcf64468f987c
Duc Anh <ducanhlekha2802@gmail.com>
2023-05-25 08:52:42 +0700

    feature_api/create_ticket_and_seat_entity

--- biztrip_api/src/main/java/com/t2104e/biztrip/entities/CoachEntity.java
+++ biztrip_api/src/main/java/com/t2104e/biztrip/entities/CoachEntity.java
@@ -51,7 +51,8 @@ public class CoachEntity {
             joinColumns = @JoinColumn(name = "coach_id"),
             inverseJoinColumns = @JoinColumn(name = "utility_id"))
     private Set<UtilityEntity> utilities;
-
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "coaches")
     private List<ThumbnailEntity> thumbnails;
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "coaches")
+    private List<SeatEntity> seats;
 }
---
+++ biztrip_api/src/main/java/com/t2104e/biztrip/entities/SeatEntity.java
@@ -0,0 +1,42 @@
+package com.t2104e.biztrip.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.util.Date;
+import java.util.List;
+
+@Builder
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity(name = "seats")
+@Table(name = "seats", schema = "biztrip_database", catalog = "")
+public class SeatEntity {
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    @Column(name = "id")
+    private long id;
+    @ManyToOne
+    @JoinColumn(name = "coach_id", nullable = false)
+    private CoachEntity coaches;
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "ticket_id")
+    private TicketEntity tickets;
+    @Basic
+    @Column(name = "seat_code", nullable = false)
+    private String seatCode;
+    @Basic
+    @Column(name = "type", nullable = false)
+    private String type;
+    @Basic
+    @Column(name = "created_at" ,columnDefinition="TIMESTAMP")
+    @DateTimeFormat(pattern = "yyyy-MM-dd H:m:s")
+    private Date createdAt;
+    @Basic
+    @Column(name = "updated_at", columnDefinition="TIMESTAMP")
+    @DateTimeFormat(pattern = "yyyy-MM-dd H:m:s")
+    private Date updatedAt;
+}
