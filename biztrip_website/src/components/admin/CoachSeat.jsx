b2d6996d3897eb06a27a96688e7af2937352c0b2
Lê Khả Đức Anh <ducanhlekha2802@gmail.com>
2023-06-08 21:35:06 +0700

    feature_website/layout_create_seat_admin_#1

--- biztrip_website/src/components/admin/CoachSeat.jsx
+++ biztrip_website/src/components/admin/CoachSeat.jsx
@@ -5,12 +5,20 @@ import {useDispatch} from "react-redux"
 import {fetchAllTicket} from "../../redux/slices/ticketSlice.jsx"
 
 const CoachSeat = () => {
-    const [selectedBox, setSelectedBox] = useState(0)
-    const [rows, setRows] = useState(8)
     const maxBoxesPerRow = 5
+    const [rows, setRows] = useState(8)
+    const [selectedBox, setSelectedBox] = useState(null)
+    const [boxType, setBoxType] = useState(null);
+    const [busLayout, setBusLayout] = useState(() =>
+        Array.from({length: rows}, () => Array.from({length: maxBoxesPerRow}, () => {
+            return {
+                type: 'space',
+                seatCode: null,
+                ticketId: 0
+            }
+        }))
+    );
     const [tickets, setTickets] = useState([])
-    const [seatData, setSeatData] = useState([])
-    const [type, setType] = useState('')
     const [ticketId, setTicketId] = useState(0)
     const [seatCode, setSeatCode] = useState('')
     const dispatch = useDispatch()
@@ -24,99 +32,114 @@ const CoachSeat = () => {
         }
         test()
     }, [])
-    useEffect(() => {
-        const data = []
-        const totalBoxes = rows * maxBoxesPerRow
-        for (let row = 0; row < rows; row++) {
-            const startBox = row * maxBoxesPerRow + 1
-            const endBox = Math.min(startBox + maxBoxesPerRow - 1, totalBoxes)
-            for (let box = startBox; box <= endBox; box++) {
-                data.push({
-                    position: box
-                })
-            }
-        }
-        setSeatData(data)
-    }, [])
+    const handleSaveSeat = (e) => {
+        e.preventDefault();
+        const updatedLayout = [...busLayout];
+        updatedLayout[selectedBox[0]][selectedBox[1]] = {
+            type: boxType,
+            seatCode: boxType === 'seat' ? seatCode : null,
+            ticketId: boxType === 'seat' ? ticketId : null,
+        };
 
-    const handleBoxClick = (box) => {
-        console.log(seatData)
-        setSelectedBox(box)
-        setTicketId(0)
-        setType('')
-        setSeatCode('')
+        setBusLayout(updatedLayout);
+        setSelectedBox(null);
+        setBoxType(null)
+        setTicketId(0);
+        setSeatCode('');
+    };
+    const handleBoxClick = (rowIndex, columnIndex) => {
+        const selectedBoxData = busLayout[rowIndex][columnIndex];
+        setSelectedBox([rowIndex, columnIndex]);
+        setBoxType(selectedBoxData.type);
+        setSeatCode(selectedBoxData.seatCode || '');
+        setTicketId(selectedBoxData.ticketId || 0);
+    };
+    const handleCreateButtonClick = (e) => {
+        e.preventDefault();
 
+        const dataToSend = busLayout.flatMap((row, rowIndex) =>
+            row.map((position, columnIndex) => {
+                const positionIndex = rowIndex * maxBoxesPerRow + columnIndex + 1;
+                return {
+                    ...position,
+                    position: positionIndex,
+                };
+
+            })
+        );
+        console.log(dataToSend);
+    };
+
+    const handleAddRow = (e) => {
+        e.preventDefault()
+        setBusLayout((prevLayout) => {
+            const newLayout = [...prevLayout];
+            const newRow = Array.from({length: maxBoxesPerRow}, () => {
+                return {
+                    type: "space",
+                    seatCode: null,
+                    ticketId: 0,
+                };
+            });
+            newLayout.push(newRow);
+            return newLayout;
+        });
+        setRows((prevRows) => prevRows + 1);
     }
-    const renderRows = () => {
-        const rowsArray = []
-        const totalBoxes = rows * maxBoxesPerRow
+    const handleRemoveLastRow = (e) => {
+        e.preventDefault();
+        const newRow = rows - 1;
+        if (newRow < 4) {
+            toast.error("Tối thiểu phải có 5 hàng");
+        } else {
+            setBusLayout((prevLayout) => {
+                const updatedLayout = [...prevLayout];
+                updatedLayout.pop();
+                return updatedLayout;
+            });
+            setRows(newRow);
+        }
+    };
+    useEffect(() => {
+        renderCoachLayout()
+    }, [rows])
+    const renderCoachLayout = () => {
+        const layout = [];
         for (let row = 0; row < rows; row++) {
-            const startBox = row * maxBoxesPerRow + 1
-            const endBox = Math.min(startBox + maxBoxesPerRow - 1, totalBoxes)
-            const boxes = []
-
-            for (let box = startBox; box <= endBox; box++) {
-                boxes.push(
+            const columns = [];
+            for (let column = 0; column < maxBoxesPerRow; column++) {
+                const position = busLayout[row][column];
+                columns.push(
                     <div
-                        key={box}
-                        className={`border-2 w-16 h-16 cursor-pointer bg-white hover:bg-primaryColor hover:text-white transition-all duration-300 ease-in-out mr-1.5 last:mr-0 relative`}
-                        onClick={() => handleBoxClick(box)}>
-                       <span className={`absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2`}>
-                           {box}
+                        key={`${row}-${column}`}
+                        className={`border-2 w-16 h-16 cursor-pointer bg-white hover:bg-primaryColor hover:text-white transition-all duration-300 ease-in-out mr-1.5 last:mr-0 relative ${position.type}`}
+                        onClick={() => handleBoxClick(row, column)}
+                    >
+                        <span className={`absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2`}>
+                            {position.type === 'space' ? `${row * maxBoxesPerRow + column + 1}` :
+                                <Icon type={position.type}/>}
                        </span>
                     </div>
-                )
+                );
             }
-            rowsArray.push(
-                <div key={row}
-                     className={`flex justify-center items-center mb-1.5 ${row === rows - 1 ? 'relative' : ''}`}>
-                    {
-                        row === rows - 1
-                            ? <span onClick={(e) => handleRemoveLastRow(e)}
-                                    className={`absolute -left-8 cursor-pointer`}>
+
+            layout.push(<div key={row}
+                             className={`flex justify-center items-center mb-1.5 ${row === rows - 1 ? 'relative' : ''}`}>
+                {
+                    row === rows - 1
+                        ? <span onClick={(e) => handleRemoveLastRow(e)}
+                                className={`absolute -left-8 cursor-pointer`}>
                                 <AiFillMinusCircle
                                     className={`w-6 h-6 text-dangerColor-default_2 hover:text-dangerColor-hover_2 duration-300 transition-all ease-in-out`}/>
                             </span>
-                            : null
-                    }
-                    {boxes}
-                </div>
-            )
-        }
-        return rowsArray
-    }
-    const handleAddRow = (e) => {
-        e.preventDefault()
-        setRows(rows + 1)
-    }
-    const handleRemoveLastRow = (e) => {
-        e.preventDefault()
-        const newRow = rows - 1
-        if (newRow <= 4) {
-            toast.error("Tối thiểu phải có 5 hàng")
-            setRows(rows)
-        } else {
-            setRows(newRow)
-        }
-    }
-    const handleSaveSeat = (e) => {
-        e.preventDefault()
-        setSeatData((seatData) =>
-            seatData.map((item) => {
-                if (item.position === selectedBox) {
-                    return {
-                        ...item,
-                        seatCode: seatCode,
-                        ticketId: ticketId,
-                        type: type
-                    }
+                        : null
                 }
-                return {
-                    ...item
-                }
-            })
-        )
-    }
+                {columns}
+            </div>);
+        }
+        return layout;
+    };
+
     return (
         <div className={`overflow-x-auto`}>
             <div className={`inline-block min-w-full align-middle`}>
@@ -136,29 +159,25 @@ const CoachSeat = () => {
                             <div className={`w-max bg-white border-4 relative rounded-4xl px-10 pb-8 pt-20`}>
                                 <span
                                     className={`absolute bg-gray-300 w-full h-14 top-0 right-0 left-0 rounded-t-4xl border-4`}></span>
-                                {renderRows()}
+                                {renderCoachLayout()}
                             </div>
                         </div>
                         {
-                            selectedBox != null
-                                ? <div className={`col-span-1 flex justify-center`}>
+                            selectedBox !== null && (
+                                <div className={`col-span-1 flex justify-center`}>
                                     <div className={`w-max h-max bg-white border-2 p-4`}>
                                         <div className={`flex flex-col w-96 mb-4`}>
-                                            <label htmlFor={`position`}>Vị trí ô</label>
-                                            <input id={`position`}
-                                                   type={`text`}
-                                                   readOnly={true}
-                                                   defaultValue={+selectedBox ? +selectedBox : ''}/>
-                                        </div>
-                                        <div className={`flex flex-col w-96 mb-4`}>
-                                            <label htmlFor={`type`}>Loại vị trí</label>
-                                            <select onChange={(e) => setType(e.target.value)}
-                                                    id={`type`}
-                                                    value={type && type.length > 0 ? type : ''}>
-                                                <option value={``}>Chọn</option>
-                                                <option value={`driver`}>Ghế cho tài xế</option>
-                                                <option value={`customer`}>Ghế cho hành khách</option>
-                                                <option value={`space`}>Khoảng trống</option>
+                                            <h3>Select Position Type</h3>
+                                            <label htmlFor="position-type">Type:</label>
+                                            <select
+                                                id="position-type"
+                                                value={boxType || ''}
+                                                onChange={(e) => setBoxType(e.target.value)}
+                                            >
+                                                <option value="">Select Type</option>
+                                                <option value="seat">Seat</option>
+                                                <option value="space">Space</option>
+                                                <option value="driver">Driver</option>
                                             </select>
                                         </div>
                                         <div className={`flex flex-col w-96 mb-4`}>
@@ -171,7 +190,7 @@ const CoachSeat = () => {
                                         <div className={`flex flex-col w-96 mb-4`}>
                                             <label htmlFor={`ticketId`}>Giá vé</label>
                                             <select id={`ticketId`}
-                                                value={+ticketId ? +ticketId : 0}
+                                                    value={+ticketId ? +ticketId : 0}
                                                     onChange={(e) => setTicketId(+e.target.value)}>
                                                 <option value={0}>Chọn</option>
                                                 {
@@ -193,13 +212,18 @@ const CoachSeat = () => {
                                         </div>
                                     </div>
                                 </div>
-                                : null
-                        }
+                            )}
                     </div>
                 </div>
             </div>
+            <div>
+                <button onClick={handleCreateButtonClick}>Tạo ghế</button>
+            </div>
         </div>
     )
-}
+};
 
+const Icon = ({type}) => {
+    return <div>{type === 'seat' ? 'Seat Icon' : 'Driver Icon'}</div>;
+};
 export default CoachSeat
