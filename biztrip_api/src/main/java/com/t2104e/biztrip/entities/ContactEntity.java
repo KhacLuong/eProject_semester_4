3f4a24bd9b6378456b94cb5291ac960df3d12262
Duc Anh <ducanhlekha2802@gmail.com>
2023-06-22 21:12:55 +0700

    hot_fix/fix_contact_field

--- biztrip_api/src/main/java/com/t2104e/biztrip/entities/ContactEntity.java
+++ biztrip_api/src/main/java/com/t2104e/biztrip/entities/ContactEntity.java
@@ -19,20 +19,20 @@ public class ContactEntity {
     @Column(name = "id")
     private long id;
     @Basic
-    @Column(name = "first_name", nullable = false)
-    private String firstName;
+    @Column(name = "full_name")
+    private String fullName;
     @Basic
-    @Column(name = "last_name", nullable = false)
-    private String lastName;
-    @Basic
-    @Column(name = "email", nullable = false)
+    @Column(name = "email")
     private String email;
     @Basic
-    @Column(name = "phone_number", nullable = false)
+    @Column(name = "phone_number")
     private String phoneNumber;
     @Basic
-    @Column(name = "status", nullable = false)
+    @Column(name = "status")
     private boolean status;
+    @Basic
+    @Column(name = "title", nullable = false)
+    private String title;
     @Basic
     @Column(name = "content", nullable = false, columnDefinition = "text")
     private String content;
